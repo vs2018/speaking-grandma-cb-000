6143cd17da4b8e54628a4c9df05520c34bb12372
Vishal Sharma <vsh1819@gmail.com>
2018-06-06 16:40:56 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(string)
   if(string == string.upcase)
     return "I LOVE YOU TOO PUMPKIN!"
-  else
+  elsif(string != string.upcase)
     return "HUH?! SPEAK UP, SONNY!"
   end
   
